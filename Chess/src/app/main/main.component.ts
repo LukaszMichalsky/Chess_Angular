85f7136d1325f2368c4709f9ceae4ffeb3bdf3eb
Lukasz Michalski <lukaso.0710@gmail.com>
2021-10-27 15:06:07 +0200

    Some changes

--- Chess/src/app/main/main.component.ts
+++ Chess/src/app/main/main.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
+  selector: 'app-main-component',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+   
+  vertical_index: number[] = [];
+  horizontal_index: String[] =[];
 
-  constructor() { }
-
+  
+  constructor() {
+    for(let i=1;i<9;i++)
+    {
+      this.horizontal_index[i-1]=String.fromCharCode(i+64);
+      this.vertical_index[i-1]=i;
+    }//stworzenie i zainicjalizowanie 2 tablic dla indeksow bo latwiej niz wpisywac recznie tablice dla ngfor
+    //obie sa w kolejnosci roznacej ale vert index wyswietlic odwrotnie
+  }
   ngOnInit(): void {
   }
 
