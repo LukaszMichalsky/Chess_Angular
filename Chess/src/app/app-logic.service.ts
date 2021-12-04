c40a71ade20f44bfd0e256895bc4720a6090c960
LukaszMichalsky <43893107+LukaszMichalsky@users.noreply.github.com>
2021-12-04 12:43:32 +0100

    Add service

--- Chess/src/app/app-logic.service.ts
+++ Chess/src/app/app-logic.service.ts
@@ -1,13 +1,180 @@
 import { Injectable } from '@angular/core';
+import { Color, IPiece, PieceType } from './models/pieces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppLogicService {
 
+  chessboard: IPiece[][];
+  PlayerTurn = Color.White;//true if white player turn / false if black player turn
   constructor() {
-
+    this.chessboard = [];
+    for(let i=0;i<8;i++)
+    {
+      this.chessboard[i] = [];
+      for(let j=0;j<8;j++)
+      {
+        this.chessboard[i][j]= {
+          "row":i,
+          "column":j,
+          "validCell":false
+        };
+      }
+    }
+    this.setPieces();
    }
+   //-----------------------FUNCTIONS------------------------
+  setPieces()//pierwsze ustawienie figur
+  {
+    for(let i=0;i<8;i++)//pawns
+    {
+      this.chessboard[1][i] = {
+        "type": PieceType.Pawn,
+        "color": Color.Black,
+        "row": 1,
+        "column": i,
+        "validCell": false
+      };
+      this.chessboard[6][i] = {
+        "type": PieceType.Pawn,
+        "color": Color.White,
+        "row": 6,
+        "column": i,
+        "validCell": false
+      }
+    }
+     //ROOKS
+     this.chessboard[0][0]={"type": PieceType.Rook,"color": Color.Black,"row": 0,"column":0,"validCell": false};
+     this.chessboard[0][7]={"type": PieceType.Rook,"color": Color.Black,"row": 0,"column":7,"validCell": false};
+     this.chessboard[7][0]={"type": PieceType.Rook,"color": Color.White,"row": 7,"column":0,"validCell": false};
+     this.chessboard[7][7]={"type": PieceType.Rook,"color": Color.White,"row": 7,"column":7,"validCell": false};
+     //KNIGHTS
+     this.chessboard[0][1]={"type": PieceType.Knight,"color": Color.Black,"row": 0,"column":1,"validCell": false};
+     this.chessboard[0][6]={"type": PieceType.Knight,"color": Color.Black,"row": 0,"column":6,"validCell": false};
+     this.chessboard[7][1]={"type": PieceType.Knight,"color": Color.White,"row": 7,"column":1,"validCell": false};
+     this.chessboard[7][6]={"type": PieceType.Knight,"color": Color.White,"row": 7,"column":6,"validCell": false};
+     //BISHOPS
+     this.chessboard[0][2]={"type": PieceType.Bishop,"color": Color.Black,"row": 0,"column":2,"validCell": false};
+     this.chessboard[0][5]={"type": PieceType.Bishop,"color": Color.Black,"row": 0,"column":5,"validCell": false};
+     this.chessboard[7][2]={"type": PieceType.Bishop,"color": Color.White,"row": 7,"column":2,"validCell": false};
+     this.chessboard[7][5]={"type": PieceType.Bishop,"color": Color.White,"row": 7,"column":5,"validCell": false};
+     //QUEENS
+     this.chessboard[0][4]={"type": PieceType.Queen,"color": Color.Black,"row": 0,"column":4,"validCell": false};
+     this.chessboard[7][3]={"type": PieceType.Queen,"color": Color.White,"row": 7,"column":3,"validCell": false};
+     //KINGS
+     this.chessboard[0][3]={"type": PieceType.King,"color": Color.Black,"row": 0,"column":3,"validCell": false};
+     this.chessboard[7][4]={"type": PieceType.King,"color": Color.White,"row": 7,"column":4,"validCell": false};
+  }
+  clearValidMoves(){
+    for(let row of this.chessboard)
+      for(let cell of row)
+          cell.validCell=false;
+  }
+  setValidCell(row: number,col: number,color: any)
+  {
+    let val = this.chessboard[row][col].color;
+    if(val == undefined)
+    {
+      this.chessboard[row][col].validCell=true;//without piece set validcell
+      return false;
+    }
+    else if(val != color)
+    {
+      this.chessboard[row][col].validCell=true;
+      return true;
+    }
+    return true;//-> if(fnc) braeak
+  }
+  changePlayerTurn()
+  {
+    if(this.PlayerTurn==Color.White)
+      this.PlayerTurn=Color.Black;
+    else
+      this.PlayerTurn=Color.White;
+  }
+  //==============================Pieces moveset functions==========================
+  PawnMovesSet(tab: IPiece){}
 
+  RookMoveSet(tab: IPiece)
+  {
+    for(let i=tab.row-1;i>=0;i--)//from piece to up
+        {
+          if(this.setValidCell(i,tab.column,tab.color))
+            break;
+        }
+        for(let i=tab.row+1;i<8;i++)//from piece to down
+        {
+          if(this.setValidCell(i,tab.column,tab.color))
+            break;
+        }
+        for(let i=tab.column-1;i>=0;i--)//from piece to left
+        {
+          if(this.setValidCell(tab.row,i,tab.color))
+            break;
+        }
+        for(let i=tab.column+1;i<8;i++)//from piece to to right
+        {
+          if(this.setValidCell(tab.row,i,tab.color))
+            break;
+        }
+  }
+
+  KnightMoveSet(tab: IPiece)
+  {
+    let j:number;
+        for(let i=-2;i<=2;i++)
+        {
+          if(i==0) continue;
+          if(i%2==0) j=-1;
+          else j=-2;
+          for(let k=0;k<2;k++)
+          {
+            if(tab.row+i>=0 && tab.row+i<8 && tab.column+j>=0 && tab.column+j<8 )//is in array range
+            {
+              this.setValidCell(tab.row+i,tab.column+j,tab.color);
+            }
+            j=j*-1;
+          }
+        }
+  }
+
+  BishopMoveSet(tab:IPiece)
+  {
+    for(let i=1;i<8;i++)//diagonal up left
+    {
+      if(tab.row-i<0 || tab.column-i<0 || this.setValidCell(tab.row-i,tab.column-i,tab.color))
+        break;
+    }
+    for(let i=1;i<8;i++)//diagonal up right
+    {
+      if(tab.row-i<0 || tab.column+i>7 || this.setValidCell(tab.row-i,tab.column+i,tab.color))
+        break;
+    }
+    for(let i=1;i<8;i++)//diagonal down right
+    {
+      if(tab.row+i>7 || tab.column+i>7 || this.setValidCell(tab.row+i,tab.column+i,tab.color))
+        break;
+    }
+    for(let i=1;i<8;i++)//diagonal down left
+    {
+      if(tab.row+i>7 || tab.column-i<0 || this.setValidCell(tab.row+i,tab.column-i,tab.color))
+        break;
+    }
+  }
+
+  KingMoveSet(tab: IPiece)
+  {
+    for(let i=-1;i<2;i++)
+    {
+      for(let j=-1;j<2;j++)
+      {
+        if(tab.row+i>0 && tab.row+i<8 && tab.column+i>0 && tab.column+i<8 )//is in array range
+        {
+          this.setValidCell(tab.row+i,tab.column+j,tab.color);
+        }
+      }
+    }
+  }
 
 }
--- Chess/src/app/main/main.component.ts
+++ Chess/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { AppLogicService } from '../app-logic.service';
 import { IPiece,Color,PieceType } from '../models/pieces';
 
 @Component({
@@ -9,29 +10,30 @@ import { IPiece,Color,PieceType } from '../models/pieces';
 export class MainComponent implements OnInit {
   vertical_index: number[] = [];
   horizontal_index: String[] =[];
-  chessboard: IPiece[][];
-  validCell: boolean=false;
-  PlayerTurn = Color.White;//true if white player turn / false if black player turn
+  //  chessboard: IPiece[][];
+  validCell: boolean=false
+
   previousClick: IPiece={
     row: -1,
     column: -1,
     validCell: false
   };
-  constructor() {
 
-    this.chessboard = [];
-    for(let i=0;i<8;i++)
-    {
-      this.chessboard[i] = [];
-      for(let j=0;j<8;j++)
-      {
-        this.chessboard[i][j]= {
-          "row":i,
-          "column":j,
-          "validCell":false
-        };
-      }
-    }
+  constructor(private appLogicService: AppLogicService) {
+
+    // this.chessboard = [];
+    // for(let i=0;i<8;i++)
+    // {
+    //   this.chessboard[i] = [];
+    //   for(let j=0;j<8;j++)
+    //   {
+    //     this.chessboard[i][j]= {
+    //       "row":i,
+    //       "column":j,
+    //       "validCell":false
+    //     };
+    //   }
+    //}
 
   }
 
@@ -46,151 +48,91 @@ export class MainComponent implements OnInit {
     //stworzenie i zainicjalizowanie 2 tablic dla indeksow bo latwiej niz wpisywac recznie tablice dla ngfor
     //obie sa w kolejnosci roznacej ale vert index wyswietlic odwrotnie
     //odwrocenie bo na odwrot trzeba wyswietlic dolny lewy a1 dla bialych na dole
-    this.setPieces();
+    // this.appLogicService.setPieces();
   }
 
-  setPieces()//pierwsze ustawienie figur
-  {
-    for(let i=0;i<8;i++)//pawns
-    {
-      this.chessboard[1][i] = {
-        "type": PieceType.Pawn,
-        "color": Color.Black,
-        "row": 1,
-        "column": i,
-        "validCell": false
-      };
-      this.chessboard[6][i] = {
-        "type": PieceType.Pawn,
-        "color": Color.White,
-        "row": 6,
-        "column": i,
-        "validCell": false
-      }
-    }
-    //ROOKS
-    this.chessboard[0][0]={"type": PieceType.Rook,"color": Color.Black,"row": 0,"column":0,"validCell": false};
-    this.chessboard[0][7]={"type": PieceType.Rook,"color": Color.Black,"row": 0,"column":7,"validCell": false};
-    this.chessboard[7][0]={"type": PieceType.Rook,"color": Color.White,"row": 7,"column":0,"validCell": false};
-    this.chessboard[7][7]={"type": PieceType.Rook,"color": Color.White,"row": 7,"column":7,"validCell": false};
-    //KNIGHTS
-    this.chessboard[0][1]={"type": PieceType.Knight,"color": Color.Black,"row": 0,"column":1,"validCell": false};
-    this.chessboard[0][6]={"type": PieceType.Knight,"color": Color.Black,"row": 0,"column":6,"validCell": false};
-    this.chessboard[7][1]={"type": PieceType.Knight,"color": Color.White,"row": 7,"column":1,"validCell": false};
-    this.chessboard[7][6]={"type": PieceType.Knight,"color": Color.White,"row": 7,"column":6,"validCell": false};
-    //BISHOPS
-    this.chessboard[0][2]={"type": PieceType.Bishop,"color": Color.Black,"row": 0,"column":2,"validCell": false};
-    this.chessboard[0][5]={"type": PieceType.Bishop,"color": Color.Black,"row": 0,"column":5,"validCell": false};
-    this.chessboard[7][2]={"type": PieceType.Bishop,"color": Color.White,"row": 7,"column":2,"validCell": false};
-    this.chessboard[7][5]={"type": PieceType.Bishop,"color": Color.White,"row": 7,"column":5,"validCell": false};
-    //QUEENS
-    this.chessboard[0][4]={"type": PieceType.Queen,"color": Color.Black,"row": 0,"column":4,"validCell": false};
-    this.chessboard[7][3]={"type": PieceType.Queen,"color": Color.White,"row": 7,"column":3,"validCell": false};
-    //KINGS
-    this.chessboard[0][3]={"type": PieceType.King,"color": Color.Black,"row": 0,"column":3,"validCell": false};
-    this.chessboard[7][4]={"type": PieceType.King,"color": Color.White,"row": 7,"column":4,"validCell": false};
+  // setPieces()//pierwsze ustawienie figur
+  // {
+  //   for(let i=0;i<8;i++)//pawns
+  //   {
+  //     this.chessboard[1][i] = {
+  //       "type": PieceType.Pawn,
+  //       "color": Color.Black,
+  //       "row": 1,
+  //       "column": i,
+  //       "validCell": false
+  //     };
+  //     this.chessboard[6][i] = {
+  //       "type": PieceType.Pawn,
+  //       "color": Color.White,
+  //       "row": 6,
+  //       "column": i,
+  //       "validCell": false
+  //     }
+  //   }
+  //   //ROOKS
+  //   this.chessboard[0][0]={"type": PieceType.Rook,"color": Color.Black,"row": 0,"column":0,"validCell": false};
+  //   this.chessboard[0][7]={"type": PieceType.Rook,"color": Color.Black,"row": 0,"column":7,"validCell": false};
+  //   this.chessboard[7][0]={"type": PieceType.Rook,"color": Color.White,"row": 7,"column":0,"validCell": false};
+  //   this.chessboard[7][7]={"type": PieceType.Rook,"color": Color.White,"row": 7,"column":7,"validCell": false};
+  //   //KNIGHTS
+  //   this.chessboard[0][1]={"type": PieceType.Knight,"color": Color.Black,"row": 0,"column":1,"validCell": false};
+  //   this.chessboard[0][6]={"type": PieceType.Knight,"color": Color.Black,"row": 0,"column":6,"validCell": false};
+  //   this.chessboard[7][1]={"type": PieceType.Knight,"color": Color.White,"row": 7,"column":1,"validCell": false};
+  //   this.chessboard[7][6]={"type": PieceType.Knight,"color": Color.White,"row": 7,"column":6,"validCell": false};
+  //   //BISHOPS
+  //   this.chessboard[0][2]={"type": PieceType.Bishop,"color": Color.Black,"row": 0,"column":2,"validCell": false};
+  //   this.chessboard[0][5]={"type": PieceType.Bishop,"color": Color.Black,"row": 0,"column":5,"validCell": false};
+  //   this.chessboard[7][2]={"type": PieceType.Bishop,"color": Color.White,"row": 7,"column":2,"validCell": false};
+  //   this.chessboard[7][5]={"type": PieceType.Bishop,"color": Color.White,"row": 7,"column":5,"validCell": false};
+  //   //QUEENS
+  //   this.chessboard[0][4]={"type": PieceType.Queen,"color": Color.Black,"row": 0,"column":4,"validCell": false};
+  //   this.chessboard[7][3]={"type": PieceType.Queen,"color": Color.White,"row": 7,"column":3,"validCell": false};
+  //   //KINGS
+  //   this.chessboard[0][3]={"type": PieceType.King,"color": Color.Black,"row": 0,"column":3,"validCell": false};
+  //   this.chessboard[7][4]={"type": PieceType.King,"color": Color.White,"row": 7,"column":4,"validCell": false};
 
-  }
+  // }
 
   validMoves(tab: IPiece )
   {
 
-    this.clearValidMoves();//clear whole validmoves when pick other gif !!!add color to comper to change choose movepick to the same color not to kill enemy piece
+    this.appLogicService.clearValidMoves();//clear whole validmoves when pick other gif !!!add color to comper to change choose movepick to the same color not to kill enemy piece
     switch(tab.type)
     {
       case "Pawn":{
-
+        this.appLogicService.PawnMovesSet(tab);
       }
       break;
       case "Rook":{
-        for(let i=tab.row-1;i>=0;i--)//from piece to up
-        {
-          if(this.setValidCell(i,tab.column,tab.color))
-            break;
-        }
-        for(let i=tab.row+1;i<8;i++)//from piece to down
-        {
-          if(this.setValidCell(i,tab.column,tab.color))
-            break;
-        }
-        for(let i=tab.column-1;i>=0;i--)//from piece to left
-        {
-          if(this.setValidCell(tab.row,i,tab.color))
-            break;
-        }
-        for(let i=tab.column+1;i<8;i++)//from piece to to right
-        {
-          if(this.setValidCell(tab.row,i,tab.color))
-            break;
-        }
+        this.appLogicService.RookMoveSet(tab);
       }
       break;
       case "Knight":{
-        let j:number;
-        for(let i=-2;i<=2;i++)
-        {
-          if(i==0) continue;
-          if(i%2==0) j=-1;
-          else j=-2;
-          for(let k=0;k<2;k++)
-          {
-            if(tab.row+i>=0 && tab.row+i<8 && tab.column+j>=0 && tab.column+j<8 )//is in array range
-            {
-              this.setValidCell(tab.row+i,tab.column+j,tab.color);
-            }
-            j=j*-1;
-          }
-        }
+        this.appLogicService.KnightMoveSet(tab);
       }
       break;
       case "Bishop":{
-        for(let i=1;i<8;i++)//diagonal up left
-        {
-          if(tab.row-i<0 || tab.column-i<0 || this.setValidCell(tab.row-i,tab.column-i,tab.color))
-            break;
-        }
-        for(let i=1;i<8;i++)//diagonal up right
-        {
-          if(tab.row-i<0 || tab.column+i>7 || this.setValidCell(tab.row-i,tab.column+i,tab.color))
-            break;
-        }
-        for(let i=1;i<8;i++)//diagonal down right
-        {
-          if(tab.row+i>7 || tab.column+i>7 || this.setValidCell(tab.row+i,tab.column+i,tab.color))
-            break;
-        }
-        for(let i=1;i<8;i++)//diagonal down left
-        {
-          if(tab.row+i>7 || tab.column-i<0 || this.setValidCell(tab.row+i,tab.column-i,tab.color))
-            break;
-        }
-
+        this.appLogicService.BishopMoveSet(tab);
       }
       break;
       case "Queen":{
-
+        this.appLogicService.BishopMoveSet(tab);
+        this.appLogicService.RookMoveSet(tab);
       }
       break;
       case "King":{
-        for(let i=-1;i<2;i++)
-        {
-          for(let j=-1;j<2;j++)
-          {
-            if(tab.row+i>0 && tab.row+i<8 && tab.column+i>0 && tab.column+i<8 )//is in array range
-            {
-              this.setValidCell(tab.row+i,tab.column+j,tab.color);
-            }
-          }
-        }
+       this.appLogicService.KingMoveSet(tab);
       }
       break;
     }
-    this.chessboard[tab.row][tab.column].validCell=false;
+    this.appLogicService.chessboard[tab.row][tab.column].validCell=false;
+
   }
 
   play(tab: IPiece)
   {
-    if(tab.color==this.PlayerTurn||tab.validCell==true)
+    if(tab.color==this.appLogicService.PlayerTurn||tab.validCell==true)
     {
       if(tab.validCell==false)//if it first click check validmmoves
       {
@@ -203,43 +145,43 @@ export class MainComponent implements OnInit {
         tab.type=this.previousClick.type;
         this.previousClick.color=undefined;
         this.previousClick.type=undefined;
-        this.clearValidMoves();
-        this.changePlayerTurn();
+        this.appLogicService.clearValidMoves();
+        this.appLogicService.changePlayerTurn();
       }
 
     }else
     {
-      this.clearValidMoves();
+      this.appLogicService.clearValidMoves();
     }
   }
 
-  clearValidMoves(){
-    for(let row of this.chessboard)
-      for(let cell of row)
-          cell.validCell=false;
-  }
-  setValidCell(row: number,col: number,color: any)
-  {
-    let val = this.chessboard[row][col].color;
-    if(val == undefined)
-    {
-      this.chessboard[row][col].validCell=true;//without piece set validcell
-      return false;
-    }
-    else if(val != color)
-    {
-      this.chessboard[row][col].validCell=true;
-      return true;
-    }
-    return true;//-> if(fnc) braeak
-  }
-  changePlayerTurn()
-  {
-    if(this.PlayerTurn==Color.White)
-      this.PlayerTurn=Color.Black;
-    else
-      this.PlayerTurn=Color.White;
-  }
+  // clearValidMoves(){
+  //   for(let row of this.chessboard)
+  //     for(let cell of row)
+  //         cell.validCell=false;
+  // }
+  // setValidCell(row: number,col: number,color: any)
+  // {
+  //   let val = this.chessboard[row][col].color;
+  //   if(val == undefined)
+  //   {
+  //     this.chessboard[row][col].validCell=true;//without piece set validcell
+  //     return false;
+  //   }
+  //   else if(val != color)
+  //   {
+  //     this.chessboard[row][col].validCell=true;
+  //     return true;
+  //   }
+  //   return true;//-> if(fnc) braeak
+  // }
+  // changePlayerTurn()
+  // {
+  //   if(this.PlayerTurn==Color.White)
+  //     this.PlayerTurn=Color.Black;
+  //   else
+  //     this.PlayerTurn=Color.White;
+  // }
 }
 
 
