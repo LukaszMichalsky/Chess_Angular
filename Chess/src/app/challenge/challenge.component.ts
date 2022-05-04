ce1d930387934cb9138c7ec851f824e905b718c4
LukaszMichalsky <43893107+LukaszMichalsky@users.noreply.github.com>
2022-05-04 16:30:09 +0200

    Add global styles, challange patch

--- Chess/src/app/challenge/challenge.component.ts
+++ Chess/src/app/challenge/challenge.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IPiece } from '../models/pieces';
+import { Color, IPiece, PieceType } from '../models/pieces';
 import { LogicService } from '../service/logic.service';
 
 @Component({
@@ -8,10 +8,12 @@ import { LogicService } from '../service/logic.service';
   styleUrls: ['./challenge.component.scss']
 })
 export class ChallengeComponent implements OnInit {
+readonly keysOfPiecesType: (keyof typeof PieceType)[] = <(keyof typeof PieceType)[]>Object.keys(PieceType);
 chessboard: IPiece[][] =[];
+sidePiecesPallet: Array<{type:PieceType,color:Color}> =[];
 vertical_index: number[] = [];
 horizontal_index: String[] =[];
-  constructor(private logicService: LogicService) { 
+  constructor(private logicService: LogicService) {
     this.chessboard = logicService.chessboard;
   }
 
@@ -20,12 +22,21 @@ horizontal_index: String[] =[];
     {
       this.horizontal_index[i-1]=String.fromCharCode(i+64);
       this.vertical_index[i-1]=i;
-      console.log(this.horizontal_index)
     }
     this.vertical_index.reverse();
     //stworzenie i zainicjalizowanie 2 tablic dla indeksow bo latwiej niz wpisywac recznie tablice dla ngfor
     //obie sa w kolejnosci roznacej ale vert index wyswietlic odwrotnie
     //odwrocenie bo na odwrot trzeba wyswietlic dolny lewy a1 dla bialych na dole
+
+
+
+    for (const key of this.keysOfPiecesType) {
+      this.sidePiecesPallet.push({type: PieceType[key], color: Color.White})
+      this.sidePiecesPallet.push({type: PieceType[key], color: Color.Black})
+    }
+
   }
 
 }
+
+
